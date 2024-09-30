7efe9a83cc03c57eb5b7f509c55611bddcd96add
RithulBhat <97803855+RithulBhat@users.noreply.github.com>
2024-09-29 21:59:20 -0500

    Update integrate.py

--- integrate.py
+++ integrate.py
@@ -3,20 +3,15 @@ import openai
 # Copy and paste our API key on the line below
 openai.api_key = 'YOUR_API_KEY'
 messages = [ {"role": "system", "content": 
-              "You are a intelligent assistant."} ]
+              "You are an intelligent assistant."} ]
 
 # Create an infinite loop so we can have a continuous conversation with the AI
 while True:
-    message = input("User : ")
-    if message:
-        messages.append(
-            {"role": "user", "content": message},
-        )
-      # Access the correct model of GPT for the code
+      # Access the correct model of GPT for the code and correctly syntax it for Python (TYPE EXTRA HERE)
         chat = openai.ChatCompletion.create(
             model="gpt-3.5-turbo", messages=messages
         )
     reply = chat.choices[0].message.content
-  # print whatever reply the AI comes up with into the terminal
-    print(f"ChatGPT: {reply}")
+  # print whatever reply the AI comes up with into the terminal (TYPE EXTRA HERE)
+    
     messages.append({"role": "assistant", "content": reply})
